c4d98646d7e3832d5bd2f9d5473e7c0f36c69b50
Levente Szomor <59541573+SzmrLevee@users.noreply.github.com>
2025-01-19 17:33:55 +0100

    ERS és DRS hozzáadása

---
+++ F1RacingLib/DRS.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1RacingLib
+{
+    public class DRS
+    {
+        public bool Aktiv { get; set; }   // A DRS aktív állapota
+        public bool Palyaszakasz { get; set; } // Jelzi, hogy a versenyző DRS-zónában van-e
+
+        public DRS()
+        {
+            Aktiv = false;
+            Palyaszakasz = false;
+        }
+
+        public void Kapcsol(bool aktiv)
+        {
+            if (Palyaszakasz)
+            {
+                Aktiv = aktiv;
+            }
+        }
+    }
+}
---
+++ F1RacingLib/ERS.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1RacingLib
+{
+    public class ERS
+    {
+        public int Eroforras { get; set; } // Az ERS aktuális energiája (0-100 közötti érték)
+        public bool Aktiv { get; set; }   // Az ERS aktív állapotát jelzi
+
+        public ERS()
+        {
+            Eroforras = 100; // Alapértelmezett érték
+            Aktiv = false;   // Alapértelmezett inaktív állapot
+        }
+
+        public void Hasznal(int mennyiseg)
+        {
+            Eroforras = Math.Max(0, Eroforras - mennyiseg);
+        }
+
+        public void Toltes(int mennyiseg)
+        {
+            Eroforras = Math.Min(100, Eroforras + mennyiseg);
+        }
+
+        public void Kapcsol(bool aktiv)
+        {
+            Aktiv = aktiv;
+        }
+    }
+}
